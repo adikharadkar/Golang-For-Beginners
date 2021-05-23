424f4b17247eb1861c55519e84039f8dfb585c14
adikharadkar <58508431+adikharadkar@users.noreply.github.com>
2021-05-23 19:22:03 +0530

    Create FirstProgramInGo.go

---
+++ FirstProgramInGo.go
@@ -0,0 +1,19 @@
+// Package Declaration
+// Packages are used to organize and reuse the code
+package main
+
+// Import Statement
+// Import Statements are used to import packages from Golang's Standard Library.
+import (
+	// fmt stands for 'Format' package
+	"fmt"
+)
+
+// Main Function
+func main() {
+
+	// Println() stands for 'Print Line' which is a function we use to print something.
+	// This function lies in the fmt package
+	fmt.Println("Hello World!")
+
+}
